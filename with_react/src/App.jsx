ecbc914144b402e6c9c8e8dc438027305d223815
narayaabinnadhkari <narayanabin223@gmail.com>
2023-12-31 11:21:27 +0545

    creating header

--- with_react/src/App.jsx
+++ with_react/src/App.jsx
@@ -5,9 +5,11 @@ import About from './pages/About'
 import Signin from "./pages/Signin";
 import Signup from './pages/Signup';
 import Profile from './pages/Profile'
+import Header from "./components/Header";
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
