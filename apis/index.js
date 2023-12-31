5faf425a1457487e9ca4d873862c6b71b4542ec3
narayaabinnadhkari <narayanabin223@gmail.com>
2023-12-31 15:38:10 +0545

    created signup api route and hashing password

--- apis/index.js
+++ apis/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -17,8 +18,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is listening on port 3000 ");
 });
 
-app.use("/apis/user", userRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
