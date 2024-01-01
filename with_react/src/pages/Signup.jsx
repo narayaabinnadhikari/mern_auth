6963c96f861c8297dfb528452a9f33c1dc76fc7e
narayaabinnadhkari <narayanabin223@gmail.com>
2024-01-01 08:26:47 +0545

    signin page

--- with_react/src/pages/Signup.jsx
+++ with_react/src/pages/Signup.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 
 function Signup() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false)
   const [loading, setLoading]= useState(false)
+
+  const navigate = useNavigate();
 
   const handleChange = (e) =>{
     setFormData({ ...formData, [e.target.id]: e.target.value});
@@ -28,6 +30,7 @@ function Signup() {
           setError(true);
             return;
         }
+        navigate('/signin')
     } catch (error) {
         setLoading(false)
         setError(true)
