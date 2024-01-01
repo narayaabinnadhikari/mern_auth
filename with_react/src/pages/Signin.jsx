ded9b05258d8635f09f3d4fe681c3c4a8db08961
narayaabinnadhkari <narayanabin223@gmail.com>
2024-01-01 08:57:22 +0545

    redux toolkit started

--- with_react/src/pages/Signin.jsx
+++ with_react/src/pages/Signin.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from "../redux/user/userSlice";
-// import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 // import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  // const { loading, error } = useSelector((state) => state.user);
+  const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
@@ -21,7 +21,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // dispatch(signInStart());
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -31,13 +31,13 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        // dispatch(signInFailure(data));
+        dispatch(signInFailure(data));
         return;
       }
-      // dispatch(signInSuccess(data));
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      // dispatch(signInFailure(error));
+      dispatch(signInFailure(error));
     }
   };
   return (
@@ -64,7 +64,6 @@ export default function SignIn() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
-        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont Have an account?</p>
