38d8600e717421ff17b222b96b0cb9287419af1a
Narayan Adhikari <59079004+narayaabinnadhikari@users.noreply.github.com>
2024-01-02 07:10:03 +0545

    Update Signin for correct link

--- with_react/src/pages/Signin.jsx
+++ with_react/src/pages/Signin.jsx
@@ -68,7 +68,7 @@ export default function SignIn() {
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont Have an account?</p>
-        <Link to="/sign-up">
+        <Link to="/signup">
           <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
